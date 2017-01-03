e04061dc5f00fc7c8123d07c3e3e53ca03a34e7e
Camillo <cangelozzi@gmail.com>
2017-01-03 19:21:16 +0100

    webpack test

--- app.js
+++ app.js
@@ -1,68 +1,64 @@
-var $ = jQuery;
+var $ = require('jquery')
+
 $(document).ready(function () {
-  "use strict";
-  
- 
-  
-  var player = 1;
-  
+  'use strict'
+
+  var player = 1
+
   $('.square').on('click', function (event) {
-    var squareSelected = $(this);
-    
+    var squareSelected = $(this)
+
     if (squareSelected.hasClass('x') || squareSelected.hasClass('o')) {
-      window.alert('already selected!');
+      window.alert('already selected!')
     } else {
       if (player === 1) {
-        squareSelected.addClass('x');
+        squareSelected.addClass('x')
         if (checkIfPlayerWon('x')) {
-          //alert('Great! Player ' + player + ' won!');
-          var win = 'Great! Player ' + player + ' won!';
-          $('.winner').append(win);
-          $('.square').hide(1200);
+          // alert('Great! Player ' + player + ' won!');
+          var win = 'Great! Player ' + player + ' won!'
+          $('.winner').append(win)
+          $('.square').hide(1200)
         } else {
-          player = 2;
+          player = 2
         }
       } else {
-        squareSelected.addClass('o');
+        squareSelected.addClass('o')
         if (checkIfPlayerWon('o')) {
          // alert('Great! Player ' + player + ' won!');
-          var win = 'Great! Player ' + player + ' won!';
-            $('.winner').append(win); 
-            $('.square').hide(1200);
+          var win = 'Great! Player ' + player + ' won!'
+          $('.winner').append(win)
+          $('.square').hide(1200)
         } else {
-          player = 1;
+          player = 1
         }
       }
     }
-  });
-   
+  })
+
   function checkIfPlayerWon (sign) {
     if ($('.sq1').hasClass(sign) && $('.sq2').hasClass(sign) && $('.sq3').hasClass(sign)) {
-      return true;
+      return true
     } else if ($('.sq4').hasClass(sign) && $('.sq5').hasClass(sign) && $('.sq6').hasClass(sign)) {
-      return true;
+      return true
     } else if ($('.sq7').hasClass(sign) && $('.sq8').hasClass(sign) && $('.sq9').hasClass(sign)) {
-      return true;                                    
+      return true
     } else if ($('.sq1').hasClass(sign) && $('.sq4').hasClass(sign) && $('.sq7').hasClass(sign)) {
-      return true;                                
+      return true
     } else if ($('.sq2').hasClass(sign) && $('.sq5').hasClass(sign) && $('.sq8').hasClass(sign)) {
-      return true;
+      return true
     } else if ($('.sq3').hasClass(sign) && $('.sq6').hasClass(sign) && $('.sq9').hasClass(sign)) {
-      return true;                                    
+      return true
     } else if ($('.sq1').hasClass(sign) && $('.sq5').hasClass(sign) && $('.sq9').hasClass(sign)) {
-      return true;
+      return true
     } else if ($('.sq3').hasClass(sign) && $('.sq5').hasClass(sign) && $('.sq7').hasClass(sign)) {
-      return true;                                    
+      return true
     } else {
-      return false;                        
+      return false
     }
   }
-  
-});
+})
 
 $('.playAgain').click(function () {
-  document.location.reload(true);                    
-});
-
-
+  document.location.reload(true)
+})
 
